da67c27db6a86635b6397a548b5bb2d3b6f0f0eb
Developer-Zahid <coderexpertzahid@gmail.com>
2021-11-09 17:25:28 +0600

    Added Header Modals

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,18 @@
         // preLoader();
 		// headerHeightFixer();
     });
+	window.addEventListener('load', function() {
+		var forms = document.getElementsByClassName('needs-validation');
+		var validation = Array.prototype.filter.call(forms, function(form) {
+		  form.addEventListener('submit', function(event) {
+			if (form.checkValidity() === false) {
+			  event.preventDefault();
+			  event.stopPropagation();
+			}
+			form.classList.add('was-validated');
+		  }, false);
+		});
+	  }, false);
 
 	/* Fixed Header on Scroll function */
 	$(window).on("scroll", function () {
